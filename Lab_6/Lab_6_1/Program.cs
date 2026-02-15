ca46f88c7d5b5115d4977426f6b4e50277cfddb1
csavkomykola-cell <c.savko.mykola@student.uzhnu.edu.ua>
2026-02-15 23:09:07 +0200

    Add files via upload

--- Lab_6/Lab_6_1/Program.cs
+++ Lab_6/Lab_6_1/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 namespace Lab6
 {
+    interface Refuelable
+    {
+        void Refill();
+    }
     abstract class Vehicle
     {
         protected string brand;
@@ -12,12 +16,16 @@ namespace Lab6
         }
         public abstract void Move();
     }
-    class Car : Vehicle
+    class Car : Vehicle, Refuelable
     {
         public Car(string brand, int speed) : base(brand, speed) { }
         public override void Move()
         {
             Console.WriteLine($"Car {brand} drives at {speed} km/h");
+        }
+        public void Refill()
+        {
+            Console.WriteLine($"Car {brand} is refueling");
         }
     }
     class Bicycle : Vehicle
@@ -28,12 +36,16 @@ namespace Lab6
             Console.WriteLine($"Bicycle {brand} rides at {speed} km/h");
         }
     }
-    class Airplane : Vehicle
+    class Airplane : Vehicle, Refuelable
     {
         public Airplane(string brand, int speed) : base(brand, speed) { }
         public override void Move()
         {
             Console.WriteLine($"Airplane {brand} flies at {speed} km/h");
+        }
+        public void Refill()
+        {
+            Console.WriteLine($"Airplane {brand} is refueling");
         }
     }
     class Program
@@ -49,6 +61,10 @@ namespace Lab6
             foreach (Vehicle v in list)
             {
                 v.Move();
+                if (v is Refuelable r)
+                {
+                    r.Refill();
+                }
             }
         }
     }
