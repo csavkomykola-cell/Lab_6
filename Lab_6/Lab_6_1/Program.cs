d72085ee2624529c91d1b180702f2c5096603807
csavkomykola-cell <c.savko.mykola@student.uzhnu.edu.ua>
2026-02-15 22:57:25 +0200

    Add files via upload

---
+++ Lab_6/Lab_6_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Lab6
+{
+    abstract class Vehicle
+    {
+        protected string brand;
+        protected int speed;
+        public Vehicle(string brand, int speed)
+        {
+            this.brand = brand;
+            this.speed = speed;
+        }
+        public abstract void Move();
+    }
+    class Car : Vehicle
+    {
+        public Car(string brand, int speed) : base(brand, speed) { }
+
+        public override void Move()
+        {
+            Console.WriteLine($"Car {brand} drives at {speed} km/h");
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public Bicycle(string brand, int speed) : base(brand, speed) { } 
+        public override void Move()
+        {
+            Console.WriteLine($"Bicycle {brand} rides at {speed} km/h");
+        }
+    }
+    class Airplane : Vehicle
+    {
+        public Airplane(string brand, int speed) : base(brand, speed) { }
+
+        public override void Move()
+        {
+            Console.WriteLine($"Airplane {brand} flies at {speed} km/h");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car("Ferrari", 150);
+            Bicycle b = new Bicycle("Ukraine", 30);
+            Airplane a = new Airplane("Boeing", 900);
+            c.Move();
+            b.Move();
+            a.Move();
+        }
+    }
+}
